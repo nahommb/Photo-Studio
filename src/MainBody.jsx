77baecfd3a55c66e363b68fea453a47c8a00a5e9
Nahom Melese <nahomjr17@gmail.com>
2024-02-14 16:41:20 -0800

    some changes

--- src/MainBody.jsx
+++ src/MainBody.jsx
@@ -2,11 +2,15 @@ import React from "react";
 function MainBody(){
 
     return <section className="middle-section">
-    <div>
-        <h2>lionel messi</h2>
+    <div style={{textAlign:'end'}}>
+                    <h1 style={{fontSize:"45px",color:'red',}}> PROPERTY </h1>
+                    <h1 style={{fontSize:'45px',color:"silver"}}>TYPES</h1>
+                    <p>Properties you can<br/> reserve on Yegna <br/> Home.</p>
     </div>
         <div className="bodyy">
-                <div className="dv1">1</div>
+                <div className="dv1">
+                    
+                </div>
                 <div className="dv1">2</div>
                 <div className="dv1">3</div>
                 <div className="dv1">4</div>
