8cb17d81148d043418688269ae3d5bbec531a095
Nahom Melese <nahomjr17@gmail.com>
2024-02-14 16:05:42 -0800

    some changes

--- src/MainBody.jsx
+++ src/MainBody.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 function MainBody(){
 
-    return <section style={{display:'flex'}}>
-    <div style={{height:'100px',width:"100px" ,backgroundColor:'red'}}>
+    return <section className="middle-section">
+    <div>
         <h2>lionel messi</h2>
     </div>
         <div className="bodyy">
