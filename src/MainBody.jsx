a0f674e64eda593c24f62027e459a6ae8dbb49f2
Nahom Melese <nahomjr17@gmail.com>
2024-02-16 17:19:51 -0800

    some changes

--- src/MainBody.jsx
+++ src/MainBody.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 function MainBody(){
-
+   const list = [
+    'lee',
+    'dude',
+    'andress',
+    'garnacho'
+   ]
     return <section className="middle-section">
     <div style={{textAlign:'end'}}>
                     <h1 style={{fontSize:"45px",color:'red',}}> PROPERTY </h1>
@@ -8,12 +13,9 @@ function MainBody(){
                     <p>Properties you can<br/> reserve on Yegna <br/> Home.</p>
     </div>
         <div className="bodyy">
-                <div className="dv1">
-                    
-                </div>
-                <div className="dv1">2</div>
-                <div className="dv1">3</div>
-                <div className="dv1">4</div>
+               {list.map((list,index)=> 
+               <div className="dv1">{list}</div>)
+               }
 
     </div>
     </section> 
