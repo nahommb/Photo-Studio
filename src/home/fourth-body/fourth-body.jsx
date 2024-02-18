dda4ac6513e35de8c79caf4a7b0ca44076f07a4a
Nahom Melese <nahomjr17@gmail.com>
2024-02-17 16:47:14 -0800

    some changes

--- src/home/fourth-body/fourth-body.jsx
+++ src/home/fourth-body/fourth-body.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import FirstBody from "../first-body/first-body";
 
 function FourthBody(){
 
     return <section className="fourth-body">
-
+          <FirstBody></FirstBody>
     </section>
 }
 export default FourthBody;
