a78eddf901f8ec79d38009eb0834b77225e93c6a
Nahom Melese <nahomjr17@gmail.com>
2024-03-06 11:42:14 -0800

    some changes

--- src/home/fifth-body/fifth-body.jsx
+++ src/home/fifth-body/fifth-body.jsx
@@ -1,10 +1,17 @@
+import { colors } from "@mui/material";
 import React from "react";
 
 function FifthBody(){
 
     return <section className="fifth-body">
       <div className="item">
+          <div>
+         <h1 style={{fontWeight:'bold',fontSize:'26px', color:"red"}}>For never <span style={{color:'white'}}>having to</span>  compromise on adventure.</h1> 
+         <br></br>
+          <p>Live in a thousand homes and experience the world through different <br/>
+          stays that are up to your liking.</p>
           
+          </div>
       </div>
     </section>
 }
