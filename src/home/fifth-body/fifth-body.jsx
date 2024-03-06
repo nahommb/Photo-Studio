58f2dd6dd6f171e7a407a16249f39a1a252f1a52
Nahom Melese <nahomjr17@gmail.com>
2024-03-06 11:47:05 -0800

    some changes

--- src/home/fifth-body/fifth-body.jsx
+++ src/home/fifth-body/fifth-body.jsx
@@ -6,7 +6,7 @@ function FifthBody(){
     return <section className="fifth-body">
       <div className="item">
           <div>
-         <h1 style={{fontWeight:'bold',fontSize:'26px', color:"red"}}>For never <span style={{color:'white'}}>having to</span>  compromise on adventure.</h1> 
+         <p style={{fontWeight:'bolder',fontSize:'26px', color:"red"}}>For never <span style={{color:'white'}}>having to</span>  compromise on adventure.</p> 
          <br></br>
           <p>Live in a thousand homes and experience the world through different <br/>
           stays that are up to your liking.</p>
